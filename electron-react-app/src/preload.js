e489d488484cb98fb6614bf3fd6f53db198e5e56
nithinmanuel9 <nithinmanuel9@gmail.com>
2025-07-21 10:32:30 +0000

    moved the IEEE 2030.5 Client (epri C) of the testing to another repository and removed unused folders such as electron and client browser

--- electron-react-app/src/preload.js
+++
@@ -1,8 +0,0 @@
-const { contextBridge, ipcRenderer } = require('electron/renderer')
-
-contextBridge.exposeInMainWorld('electronAPI', {
-  setTest: (test) => ipcRenderer.send('set-test', test),
-  getLogs: (callback)=>ipcRenderer.on('log-msg', (event,logs)=>callback(logs)),
-  getReport: (callback)=>ipcRenderer.on('report-msg', (event,report)=>callback(report)),
-  rmEventListener:(channel)=>ipcRenderer.removeAllListeners(channel)
-})
